a07843a93f6f75dfdcd69bff187820dffa2963a2
ashimroyrupok <ashimroyrupok@gmail.com>
2025-06-12 16:26:05 +0600

    completed multiple image add functionality

--- src/Layout/Dashboard/Agent/AddProperties/AddProperties.jsx
+++ src/Layout/Dashboard/Agent/AddProperties/AddProperties.jsx
@@ -7,6 +7,8 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../../Hooks/useAxiosSecure";
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 const api_key = import.meta.env.VITE_IMAGE_HOSTING_API_KEY;
 // console.log(api_key);
@@ -14,7 +16,9 @@ const hosting_api = `https://api.imgbb.com/1/upload?key=${api_key}`;
 
 const AddProperties = () => {
   const { user } = useAuth();
-
+  const [images, setImages] = useState([]);
+  const [showImage, setShowImage] = useState([]);
+  // console.log(showImage, "showww");
   const axiosPublic = useAxiosPublic();
   const axiosSecure = useAxiosSecure();
 
@@ -34,27 +38,91 @@ const AddProperties = () => {
     formState: { errors },
   } = useForm();
 
+  const handleImage = (e) => {
+    console.log(e, "ee");
+    const files = e.target.files;
+    const length = files.length;
+
+    if (length > 0) {
+      setImages([...images, ...files]);
+      let imgURL = [];
+
+      for (let i = 0; i < length; i++) {
+        imgURL.push({ url: URL.createObjectURL(files[i]) });
+      }
+      setShowImage([...showImage, ...imgURL]);
+    }
+  };
+
+  const removeImage = (i) => {
+    const image = images.filter((img, index) => index !== i);
+    const filterImgURL = showImage.filter((img, index) => index !== i);
+    setImages(image);
+    setShowImage(filterImgURL);
+  };
+
   const onSubmit = async (data) => {
     console.log(data);
 
     try {
       const imageFile = { image: data.propertyImage[0] };
-      // console.log(imageFile);
-      const res = await axiosPublic.post(hosting_api, imageFile, {
-        headers: {
-          "content-type": "multipart/form-data",
-        },
-      });
+      console.log(imageFile);
+      // const res = await axiosPublic.post(hosting_api, imageFile, {
+      //   headers: {
+      //     "content-type": "multipart/form-data",
+      //   },
+      // });
 
-      console.log(res.data);
-      console.log(res.data?.data?.display_url);
-      const image = res.data?.data?.display_url;
-      if (res.data.success) {
+      // console.log(res.data);
+      // console.log(res.data?.data?.display_url);
+      // const image = res.data?.data?.display_url;
+
+      // image uploading process for multiple images
+      const uploadPromises = Array.from(data.propertyImage).map(
+        async (image) => {
+          console.log(showImage, "show inag");
+          const formData = new FormData();
+          formData.append("image", image);
+          console.log(formData, "data");
+
+          try {
+            const res = await axiosPublic.post(hosting_api, formData, {
+              headers: {
+                "content-type": "multipart/form-data",
+              },
+            });
+            console.log(res, "res");
+
+            if (res.data.success) {
+              return res.data.data.display_url;
+            } else {
+              throw new Error("Failed to upload image");
+            }
+          } catch (err) {
+            toast.error("Failed to upload image");
+            return null; // Return null for failed uploads
+          }
+        }
+      );
+
+      const urls = await Promise.all(uploadPromises);
+      console.log(urls, "urlss");
+
+      // Check if any upload failed
+      if (urls.includes(null)) {
+        toast.error("Some images failed to upload. Please try again.");
+        return; // Don't proceed with database insertion
+      }
+      console.log(urls.filter((url) => url));
+      // end
+
+      if (urls) {
         const propertyInfo = {
           agentName: users?.name,
           agentEmail: users?.email,
           agentImage: users?.image,
-          image: image,
+          date: new Date().toJSON().slice(0, 10),
+          images: urls.filter((url) => url), // filter out any null URLs,
           builtYear: data?.builtYear,
           bedRoom: data?.bedRoom,
           bathRoom: data?.bathRoom,
@@ -155,56 +223,6 @@ const AddProperties = () => {
               <h5 className="text-red-600"> This field is required </h5>
             )}
           </div>
-          <div className="w-full">
-            <div className="form-control w-full ">
-              <TextField
-                focused
-                id="outlined-required"
-                label="Property Image"
-                name="propertyImage"
-                type="file"
-                placeholder="your propertyImage"
-                sx={{ width: "90%", borderRadius: "10px" }}
-                {...register("propertyImage", { required: true })}
-              />
-              {errors.propertyImage && (
-                <h5 className="text-red-600"> This field is required </h5>
-              )}
-            </div>
-          </div>
-        </div>
-        <div className="flex justify-around items-center ">
-          <div className="form-control w-full ">
-            <div className="flex justify-start gap-6 items-center">
-              <TextField
-                focused
-                id="outlined-required"
-                label="Minimum Price"
-                name="minimumPrice"
-                type="number"
-                placeholder="your minimumPrice"
-                sx={{ width: "40%", borderRadius: "10px" }}
-                {...register("minimumPrice", { required: true })}
-              />
-              {errors.minimumPrice && (
-                <h5 className="text-red-600"> This field is required </h5>
-              )}
-
-              <TextField
-                focused
-                id="outlined-required"
-                label="Maximum Price"
-                name="maximumPrice"
-                type="number"
-                placeholder="your maximumPrice"
-                sx={{ width: "40%", borderRadius: "10px" }}
-                {...register("maximumPrice", { required: true })}
-              />
-              {errors.maximumPrice && (
-                <h5 className="text-red-600"> This field is required </h5>
-              )}
-            </div>
-          </div>
           <div className="form-control w-full ">
             <TextField
               focused
@@ -217,6 +235,40 @@ const AddProperties = () => {
               {...register("location", { required: true })}
             />
             {errors.location && (
+              <h5 className="text-red-600"> This field is required </h5>
+            )}
+          </div>
+        </div>
+        <div className="flex justify-around items-center ">
+          <div className="form-control w-full ">
+            <div className="flex justify-start gap-6 items-center">
+              <TextField
+                focused
+                id="outlined-required"
+                label="Minimum Price"
+                name="minimumPrice"
+                type="number"
+                placeholder="your minimumPrice"
+                sx={{ width: "90%", borderRadius: "10px" }}
+                {...register("minimumPrice", { required: true })}
+              />
+              {errors.minimumPrice && (
+                <h5 className="text-red-600"> This field is required </h5>
+              )}
+            </div>
+          </div>
+          <div className="form-control w-full ">
+            <TextField
+              focused
+              id="outlined-required"
+              label="Maximum Price"
+              name="maximumPrice"
+              type="number"
+              placeholder="your maximumPrice"
+              sx={{ width: "90%", borderRadius: "10px" }}
+              {...register("maximumPrice", { required: true })}
+            />
+            {errors.maximumPrice && (
               <h5 className="text-red-600"> This field is required </h5>
             )}
           </div>
@@ -305,6 +357,53 @@ const AddProperties = () => {
             </div>
           </div>
         </div>
+
+        {/* upload images */}
+        <section>
+          <div className="grid lg:grid-cols-4 grid-cols-2 md:grid-cols-3 sm:grid-cols-2 sm:gap-4 md:gap-4 xs:gap-4 gap-3 w-full text-[#d0d2d6] mb-4">
+            {showImage.map((image, i) => (
+              <div key={i} className="lg:h-[120px] relative">
+                <label htmlFor={i}>
+                  <img
+                    className="border h-[120px] w-full shadow-md rounded-md border-slate-500"
+                    src={image.url}
+                    alt=""
+                  />
+                </label>
+                <span
+                  onClick={() => removeImage(i)}
+                  className="p-2 z-10 cursor-pointer bg-red-600 hover:shadow-lg hover:shadow-red-600/50 text-white absolute top-1 right-1 rounded-full"
+                >
+                  x
+                </span>
+              </div>
+            ))}
+          </div>
+
+          <div>
+            <label
+              className="flex flex-col justify-center items-center h-[130px] md:h-[160px] lg:h-[170px] cursor-pointer border-2 border-dashed border-slate-500 w-full hover:border-indigo-500"
+              htmlFor="image"
+            >
+              <span>{/* <BsImage /> */}</span>
+              <span className="font-medium">Select Image</span>
+            </label>
+          </div>
+        </section>
+        <input
+          type="file"
+          multiple
+          id="image"
+          name="propertyImage"
+          className="hidden"
+          {...register("propertyImage", {
+            required: true,
+            onChange: (e) => handleImage(e),
+          })}
+        />
+        {errors.propertyImage && (
+          <h5 className="text-red-600"> This field is required </h5>
+        )}
         <div className="flex justify-center items-center w-full">
           <input
             className="btn  border-[#F2561B] bg-[#F2561B] text-white hover:bg-[#ef9473] w-full mt-6"
